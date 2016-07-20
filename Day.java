3f0cd8a9fa9a71c664a19cd4cd7c72bb53dca967
sathya-185 <jsathya185@gmail.com>
2016-07-20 12:32:19 +0530

    Create Day.java

---
+++ Day.java
@@ -0,0 +1,19 @@
+public class Day {
+	public static void main(String[] args) {
+	
+		String s3="sunday";		
+		System.out.println("enter the day");
+		Scanner din=new Scanner(System.in);
+		String Workingday=din.nextLine();
+		if(Workingday.equals(s3)){
+			System.out.println(" false ");
+			
+		}
+	else {
+		System.out.println(" true ");
+		
+		}
+
+	}
+
+}
