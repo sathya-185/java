1243db7a1ee0552af6bd620803f629150f0e2bdf
sathya-185 <jsathya185@gmail.com>
2016-07-20 11:54:04 +0530

    Create Fffa.java

---
+++ Fffa.java
@@ -0,0 +1,13 @@
+public class Fffa {
+
+	public static void  main(String[] args) {
+		int fact=1;
+		int i;
+		int n=3;
+		for(i=1;i<=n;i++){
+			fact=fact*i;
+			System.out.println("factorial of "+n+"is:"+fact);
+		}
+	}
+
+}
