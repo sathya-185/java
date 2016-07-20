9ff45849f9725b20e1a7f8a2c470244c8c45bfb7
sathya-185 <jsathya185@gmail.com>
2016-07-20 10:11:30 +0530

    Create Pos.java

---
+++ Pos.java
@@ -0,0 +1,14 @@
+public class Pos {
+public static void main(String[] args) {
+		int x=-1;
+	if(x>0)
+	{
+		System.out.println("x is positive");
+	}
+	else if (x<0)
+	{
+		System.out.println("x is negative");
+	}
+	
+	
+}}
