1c99f8ef16d29f1582f69111acbf740f86db2d77
sathya-185 <jsathya185@gmail.com>
2016-08-08 01:59:56 -0700

    Create StringRev.java

---
+++ StringRev.java
@@ -0,0 +1,18 @@
+public class StringRev {
+	
+	    public static void main(String args[]){
+	    String str = "i love india";
+	    String temp = "";
+	    String finalString = "";
+	        for(int i =str.length()-1;i>=0;i--){
+	            temp +=i!=0?str.charAt(i):str.charAt(i)+" ";
+	            if(str.charAt(i) == ' '||i==0){
+	                for(int j=temp.length()-1;j>=0;j--){
+	                    finalString += temp.charAt(j);
+	                }
+	                temp = "";
+	            }
+	        }
+	            System.out.println(finalString);
+	    }
+	}
