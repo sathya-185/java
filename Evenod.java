e2be02ebb7ad57dec37fcfd01096a45d65b07743
sathya-185 <jsathya185@gmail.com>
2016-08-09 23:58:15 -0700

    Create Evenod.java

---
+++ Evenod.java
@@ -0,0 +1,11 @@
+public class Evenod {
+	public static void main(String[] args) {
+		int x;
+		x=Integer.parseInt(args[0]);
+		if(x%2==0){
+			System.out.println("given number is even");
+		}
+		else{
+			System.out.println("number is odd");
+		}
+	}
